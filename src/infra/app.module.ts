ccd1d2f30fe12349b37bef23a6255ac20e3aad32
Felipe <flpc.dev@gmail.com>
2023-12-15 18:05:24 -0300

    refactor: folder structure

---
+++ src/infra/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common'
+import { PrismaService } from './prisma/prisma.service'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
+import { CreateAccountController } from './http/controllers/create-account.controller'
+import { AuthenticateController } from './http/controllers/authenticate.controller'
+import { CreateQuestionController } from './http/controllers/create-question.controller'
+import { FetchRecentQuestionsController } from './http/controllers/fetch-recent-questions.controller'
+
+@Module({
+	imports: [ConfigModule.forRoot({
+		validate: env => envSchema.parse(env),
+		isGlobal: true,
+	}),
+	AuthModule
+	],
+	controllers: [
+		CreateAccountController, 
+		AuthenticateController, 
+		CreateQuestionController, 
+		FetchRecentQuestionsController
+	],
+	providers: [PrismaService],
+})
+export class AppModule {}
