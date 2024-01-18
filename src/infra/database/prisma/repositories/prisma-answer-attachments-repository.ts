1ac809fec236b4ba3f49994d7f38a7e6da51212f
Felipe <flpc.dev@gmail.com>
2024-01-18 14:12:44 -0300

    feat: create prisma repositories and mappers

---
+++ src/infra/database/prisma/repositories/prisma-answer-attachments-repository.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common'
+import { AnswerAttachmentsRepository } from 'src/domain/forum/application/repositories/answer-attachments-repository'
+import { AnswerAttachment } from 'src/domain/forum/enterprise/entities/answer-attachment'
+
+@Injectable()
+export class PrismaAnswerAttachmentsRepository implements AnswerAttachmentsRepository {
+	findManyByAnswerId(answerId: string): Promise<AnswerAttachment[]> {
+		throw new Error('Method not implemented.')
+	}
+
+	deleteManyByAnswerId(answerId: string): Promise<void> {
+		throw new Error('Method not implemented.')
+	}
+}
